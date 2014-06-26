550c6ba018a701e6fabe3b758dec9f0ae8c24cb3
Rakesh Kumar Shardiwal <rakesh.shardiwal@gmail.com>
2014-06-26 19:43:13 +0530

    Version 0.03 with cpan failed report fixes

--- lib/Time/Duration/Concise/Localize.pm
+++ lib/Time/Duration/Concise/Localize.pm
@@ -9,7 +9,7 @@ extends 'Time::Duration::Concise';
 
 use Module::Runtime qw(require_module);
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 NAME
 
@@ -21,7 +21,7 @@ Time::Duration::Concise is an approach to localize concise time duration string 
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =head1 SYNOPSIS
 
