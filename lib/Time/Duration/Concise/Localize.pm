4d7417947744f079b96d2850c3637e49fdbf3185
Rakesh Kumar Shardiwal <rakesh.shardiwal@gmail.com>
2014-07-06 10:26:34 +0530

    Make file improvements, added repo and pre require modules

--- lib/Time/Duration/Concise/Localize.pm
+++ lib/Time/Duration/Concise/Localize.pm
@@ -9,7 +9,7 @@ extends 'Time::Duration::Concise';
 
 use Module::Runtime qw(require_module);
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 =head1 NAME
 
@@ -21,7 +21,7 @@ Time::Duration::Concise is an approach to localize concise time duration string 
 
 =head1 VERSION
 
-Version 0.05
+Version 0.06
 
 =head1 SYNOPSIS
 
